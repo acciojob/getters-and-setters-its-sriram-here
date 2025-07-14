826853c27ce98275d05aa25b04accc0721babbec
SRIRAM <srirammurugesan1807@gmail.com>
2025-07-14 13:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-		
-	}
-	get name(){
-		return this.name;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	set age(newAge){
-		this._age = newAge;
-	}
+ 
+  get name() {
+    return this._name;
+  }
+
+  
+  get age() {
+    return this._age;
+  }
+
+
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
+
+
 class Student extends Person {
-	study() {
-		console.log('${this.name} is studying');
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
-class Teacher extends Person {}
-teach() {
-	console.log('${this.name} is teaching');
-	}
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
